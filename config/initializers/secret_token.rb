b6b702a82129a8c6fa08e6c36907527813b253a3
yowana <y.kuvaka.09@aberdeen.ac.uk>
2011-11-22 17:37:08 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WeddingPhotoApp::Application.config.secret_token = '4973e380979975ad3e0c646802b5fb5b2d6210db0fad35f2a5bd428e377cc74fd5876872ba1849da7146d1cc27a6d6d888d73cfd41a6e657a622b18d55b96db0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+WeddingPhotoApp::Application.config.session_store :cookie_store, key: '_Wedding_photo_app_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# WeddingPhotoApp::Application.config.session_store :active_record_store
